d905ffb04dd69d6bf170d21c6e03ed30d7d83f4e
Littleuzbek <dilshod.akbarov.2003@gmail.com>
2024-08-10 00:38:30 +0500

    ok

--- app/wishes/page.js
+++ app/wishes/page.js
@@ -2,7 +2,7 @@ import Wishes from '@/components/wishes/Wishes'
 import clases from './page.module.css'
 import { Suspense } from 'react'
 
-export default function Wishes() {
+export default function WishesPage() {
   return (
     <div className={clases.wishes}>
       <Suspense fallback='Loading...'>
